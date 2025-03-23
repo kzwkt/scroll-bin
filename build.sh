7291dafed50f9d9a17f06afc11d3e1eff73cb4ec
kzwkt <9437935+kzwkt@users.noreply.github.com>
2025-03-23 11:23:33 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #
 apt update -y
-apt install -y  git gcc make ca-certificates binutils --no-install-recommends
+apt install -y  git gcc make ca-certificates binutils libc6-dev --no-install-recommends
 git clone --depth=1 https://git.suckless.org/scroll
 cd scroll
 make
